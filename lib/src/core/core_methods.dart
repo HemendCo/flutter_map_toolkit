391ea47003c476f5867c97b33cb3f2a09eb80989
FMotalleb <fmotalleb@gmail.com>
2022-08-13 11:42:17 +0430

    [Feat] mapbox tile url generator method

--- lib/src/core/core_methods.dart
+++ lib/src/core/core_methods.dart
@@ -6,3 +6,28 @@ LatLng pointFromMap(Map<String, dynamic> map) {
   final point = LatLng(asType(map['lat']), asType(map['long']));
   return point;
 }
+
+String mapBoxUrlBuilder({
+  String protocol = 'https',
+  String host = 'api.mapbox.com',
+  String basePath = 'styles',
+  String apiVer = 'v1',
+  required String style,
+  bool is2x = false,
+  required String accessToken,
+}) {
+  final result = StringBuffer(protocol)
+    ..write('://')
+    ..write(host)
+    ..write('/')
+    ..write(basePath)
+    ..write('/')
+    ..write(apiVer)
+    ..write('/')
+    ..write(style)
+    ..write('/tiles/{z}/{x}/{y}')
+    ..write(is2x ? '@2x' : '')
+    ..write('?access_token=')
+    ..write(accessToken);
+  return result.toString();
+}
